7dc9e89dcc25697613c0ed09d94661f2f22e29ec
Yashika Jayasinghe <yash166@gmail.com>
2022-11-26 21:51:56 +0000

    Pending changes exported from your codespace

--- api/FindCityTrigger/__init__.py
+++ api/FindCityTrigger/__init__.py
@@ -6,37 +6,40 @@ from starlette.background import BackgroundTask
 from starlette.responses import StreamingResponse
 
 # this is a test endpoint that consumes a mocked downstream system
+
+
 @app.get("/find_city_details")
 async def find_city_details_with_mocked_downstream(country: str, postcode: int):
-    
-    ZIPPO_MOCK = "http://localhost:5000/"       
+
+    ZIPPO_MOCK = "http://localhost:5000/"
 
     with httpx.Client() as client:
         resp = client.get(f"{ZIPPO_MOCK}{country}/{postcode}")
         print(resp.text)
-        if(resp.status_code == 200):
+        if (resp.status_code == 200):
             return resp.json()
         else:
             return 'NOT OKAY'
+
 
 @app.get("/find_city_by_post_code")
 async def find_city_resp_stream_live_downstream(country: str, postcode: int):
 
     ZIPPO_REAL = "http://api.zippopotam.us/"
-    client = httpx.AsyncClient()    
+    client = httpx.AsyncClient()
     req = client.build_request("GET", f"{ZIPPO_REAL}{country}/{postcode}")
     print(req.method)
     r = await client.send(req, stream=True)
 
     return StreamingResponse(r.aiter_text(), background=BackgroundTask(r.aclose))
 
- 
+
 @app.get("/find_city_details_async")
 async def find_city_details_with_async_client(country: str, postcode: int):
-    
-    ZIPPO_MOCK = "http://localhost:5000/"    
-    
-    client = httpx.AsyncClient()    
+
+    ZIPPO_MOCK = "http://localhost:5000/"
+
+    client = httpx.AsyncClient()
     req = client.build_request("GET", f"{ZIPPO_MOCK}{country}/{postcode}")
     print(req.method)
     r = await client.send(req, stream=True)
@@ -47,5 +50,6 @@ async def find_city_details_with_async_client(country: str, postcode: int):
     #     resp = await client.get(f"{ZIPPO_MOCK}{country}/{postcode}")
     #     return resp.json()
 
-def main(req: func.HttpRequest, context:func.Context) -> func.HttpResponse:
+
+def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     return func.AsgiMiddleware(app).handle(req)
--- test/test_find_city.py
+++ test/test_find_city.py
@@ -5,7 +5,8 @@ import requests
 from flask import Flask, jsonify
 from framework import Service
 
-BASE_FUNCTION_APP_URL = "http://localhost:7071/"
+BASE_FUNCTION_APP_URL = "https://yashikajayasinghe-stunning-funicular-vppxxv7p445hwgw6-7071.preview.app.github.dev/"
+
 
 @pytest.fixture
 def simple_service():
@@ -13,6 +14,7 @@ def simple_service():
     Provides a simple service running in the pytest process
     """
     app = Flask('simple-service')
+
     @app.route('/nz/6035')
     def main():
         response_body = {
@@ -21,28 +23,31 @@ def simple_service():
             "country abbreviation": "NZ",
             "places": [
                 {
-                "place name": "Khandallah",
-                "longitude": "174.7942",
-                "state": "",
-                "state abbreviation": "",
-                "latitude": "-41.2466"
+                    "place name": "Khandallah",
+                    "longitude": "174.7942",
+                    "state": "",
+                    "state abbreviation": "",
+                    "latitude": "-41.2466"
                 }
             ]
         }
         return jsonify(response_body)
     return Service(app, 5000)
 
+
 @pytest.mark.env("acceptance")
 def test_successfully_finding_city_details_acceptance(simple_service):
-    
-    with simple_service as service:        
-            
-        path = "find_city_details?country=nz&postcode=6035"        
-        response = requests.get(url=BASE_FUNCTION_APP_URL+path)        
+
+    with simple_service as service:
+
+        path = "find_city_details?country=nz&postcode=6035"
+        response = requests.get(url=BASE_FUNCTION_APP_URL+path)
         responseJson = json.loads(response.text)
         assert response.status_code == 200
-        assert jsonpath.jsonpath(responseJson,'$.post code')[0] == "6035"
-        assert jsonpath.jsonpath(responseJson,'$.places[0].place name')[0] == "Khandallah"
+        assert jsonpath.jsonpath(responseJson, '$.post code')[0] == "6035"
+        assert jsonpath.jsonpath(responseJson, '$.places[0].place name')[
+            0] == "Khandallah"
+
 
 @pytest.mark.env("e2e")
 def test_successfully_finding_city_details_e2e():
@@ -50,6 +55,5 @@ def test_successfully_finding_city_details_e2e():
     response = requests.get(url=BASE_FUNCTION_APP_URL+path)
     responseJson = json.loads(response.text)
     assert response.status_code == 200
-    assert jsonpath.jsonpath(responseJson,'$.post code')[0] == "6035"
-    assert jsonpath.jsonpath(responseJson,'$.places[0].place name')[0] == "Khandallah"
-       
+    assert jsonpath.jsonpath(responseJson, '$.post code')[0] == "6035"
+    assert jsonpath.jsonpath(responseJson, '$.places[0].place name')[0] == "Khandallah"
